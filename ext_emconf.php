f5229a65c80193c7321d8ea1ac14da10b7f3adf0
Alexander Bigga <alexander.bigga@slub-dresden.de>
2021-06-20 17:16:25 +0200

    Update of some constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,16 +9,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'fluid_styled_content' => '8.7.0-9.5.99',
-            'rte_ckeditor' => '8.7.0-9.5.99'
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [
-        ],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'Bigga\\BiggaSite\\' => 'Classes'
         ],
     ],
     'state' => 'stable',
